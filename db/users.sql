8d9d56930ef054cd336191534e587cd3100d5b2c
Zhomart Mukhamejanov <mzhomart@gmail.com>
2015-03-28 17:29:19 -0700

    db fix

--- db/users.sql
+++ db/users.sql
@@ -5,17 +5,16 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `email` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 (1, 'Mas Banyar', 'banyar@yahoo.com', '23235645yghgf'),
 (2, 'Mas Mapmup', 'mapmup@gmail.com', 'dncskdcndscsdcdsc'),
-(2, 'zhomart', 'zhomart@shareout.pw', 'asdasdasd'),
+(3, 'zhomart', 'zhomart@shareout.pw', 'asdasdasd'),
 (4, 'Boronong', 'borononn@yahoo.com', '032bcsjdncsdjc3223'),
 (5, 'Nadya Ek', 'nadya@yahoo.com', 'bonbon032932');
