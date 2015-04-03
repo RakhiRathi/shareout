5167f64f53bc0a83f5308b5a69318e224900c5a3
Zhomart Mukhamejanov <mzhomart@gmail.com>
2015-04-02 21:19:13 -0700

    hide user add box

---
+++ db/users.sql
@@ -0,0 +1,32 @@
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Jessica Alba','alba@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Pamela Anderson','pamela@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Johny Depp','depp@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Katherine Zeta Jones','jones@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Will Smith','smith@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Sherlock Holmes','sherlock@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Bender Rodrigues Bending','bender@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Jim Carrey','jim@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Natalie Portman','natalie@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Lord Diablo','diablo@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
+
+INSERT INTO `Users` (`id`,`name`,`email`,`password`,`updatedAt`,`createdAt`)
+VALUES (DEFAULT,'Anykey Skywalker','anykey@shareout.pw','00ea1da4192a2030f9ae023de3b3143ed647bbab','2015-04-03 04:13:07','2015-04-03 04:13:07');
